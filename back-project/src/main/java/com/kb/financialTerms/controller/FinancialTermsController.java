86ec24f636cd929d2991e2924cd6e21b719ba348
Jung Seung Min <jsm1592@gmail.com>
2024-09-26 13:55:00 +0900

    jsoup추가

--- back-project/src/main/java/com/kb/financialTerms/controller/FinancialTermsController.java
+++ back-project/src/main/java/com/kb/financialTerms/controller/FinancialTermsController.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:8081")
 @RequestMapping("/api/terms")
 public class FinancialTermsController {
     private final FinancialTermsService financialTermsService;
