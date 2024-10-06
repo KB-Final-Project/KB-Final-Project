f2c1db4fbfe34a46d77a0369018e4bf1bcd27332
YeSeul <ysbsjh2464@naver.com>
2024-10-07 01:35:06 +0900

    style: 메인화면 용어사전 UI 수정

--- front-project/src/components/Feature_7.vue
+++ front-project/src/components/Feature_7.vue
@@ -3,9 +3,9 @@
     <br><br><br><br><br>
     <div class="container text-center investDictionary" @click="goToDic">
       <div class="text-start" >
-        <h1>HOW TO</h1>
-        <h1>INVEST</h1>
-        <h4>투자의 처음과 끝을 알려드립니다.</h4>
+        <b class="title-txt">HOW TO</b><br>
+        <b class="title-txt">INVEST</b><br>
+        <b class="content-txt">투자의 A to Z를 알려드립니다</b><br>
         <br><br><br><br>
         <h4>용어사전 바로가기 <i class="ai-arrow-right"></i></h4>
         <img src="/img/how.png">
@@ -29,19 +29,26 @@
 
 .investDictionary{
   color: white;
-  padding: 100px;
+  padding: 60px;
   border: none;
   border-radius: 30px;
   background-color: rgba(68, 140, 116, 1);
   width: 1200px;
   height:400px;
 }
-h1{
-  font-weight: 700;
-}
 .investDictionary img{
   margin-top: -250px;
   margin-left: 50px;
+}
+
+.title-txt {
+  font-size: 50px;
+  line-height: 1.1; /* 글자 위아래 간격 줄이기 */
+}
+
+.content-txt {
+  font-size: 30px;
+  line-height: 2;
 }
 
 </style>
@@ -52,6 +59,6 @@ import {useRouter} from "vue-router";
 const router = useRouter();
 
 const goToDic = () => {
-  router.push('dictionary');
+  router.push('/dictionary');
 }
 </script>
