11b2c4438f3acc9652df0ed22b79f3e95fdf67a6
YeSeul <ysbsjh2464@naver.com>
2024-10-07 01:48:43 +0900

    style: 메인화면 용어사전 바로가기 이미지 위치 수정

--- front-project/src/components/Feature_7.vue
+++ front-project/src/components/Feature_7.vue
@@ -2,16 +2,19 @@
   <section>
     <br><br><br><br><br>
     <div class="container text-center investDictionary" @click="goToDic">
-      <div class="text-start" >
-        <b class="title-txt">HOW TO</b><br>
-        <b class="title-txt">INVEST</b><br>
-        <b class="content-txt">투자의 A to Z를 알려드립니다</b><br>
-        <br><br><br><br>
-        <h4>용어사전 바로가기 <i class="ai-arrow-right"></i></h4>
-        <img src="/img/how.png">
+      <div class="text-container">
+        <div class="text-content">
+          <b class="title-txt">HOW TO</b><br>
+          <b class="title-txt">INVEST</b><br>
+          <b class="content-txt">투자의 A to Z를 알려드립니다</b><br>
+          <br><br><br><br>
+          <h4>용어사전 바로가기 <i class="ai-arrow-right"></i></h4>
+        </div>
+        <div class="image-container">
+          <img src="/img/how.png" alt="How to invest image">
+        </div>
       </div>
     </div>
-    <br> <br> <br> <br>
     <br><br><br><br><br>
   </section>
 </template>
@@ -33,17 +36,39 @@
   border: none;
   border-radius: 30px;
   background-color: rgba(68, 140, 116, 1);
-  width: 1200px;
-  height:400px;
+  width: 100%;
+  max-width: 1200px;
+  height: auto;
 }
-.investDictionary img{
-  margin-top: -250px;
-  margin-left: 50px;
+
+.text-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  position: relative;
+}
+
+.text-content {
+  text-align: left;
+}
+
+.image-container {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  margin-right: -100px;
+  margin-bottom: -50px;
+}
+
+.image-container img {
+  height: auto;
+  object-fit: contain;
 }
 
 .title-txt {
   font-size: 50px;
-  line-height: 1.1; /* 글자 위아래 간격 줄이기 */
+  line-height: 1.1;
 }
 
 .content-txt {
@@ -51,6 +76,17 @@
   line-height: 2;
 }
 
+@media (min-width: 768px) {
+  .text-container {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
+  }
+
+  .image-container img {
+    width: 80%;
+  }
+}
 </style>
 
 <script setup>
