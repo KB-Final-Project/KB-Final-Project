8438bdf6a5517b1915f7b0a6b4413c8055eb9f02
Jung Seung Min <jsm1592@gmail.com>
2024-10-08 14:11:05 +0900

    funds테이블1개로변경

--- back-project/src/main/java/com/kb/funds/mapper/FundsMapper.java
+++ back-project/src/main/java/com/kb/funds/mapper/FundsMapper.java
@@ -1,19 +1,13 @@
 package com.kb.funds.mapper;
-
 import com.kb.funds.dto.FundsDTO;
-import com.kb.funds.dto.SuikChartDTO;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
-
 public interface FundsMapper {
     @Transactional
     void insertFund(FundsDTO fund);
-    void updateFund(FundsDTO fund); // 업데이트 메서드 추가
-
+    void updateFund(FundsDTO fund); // 펀드 업데이트 메서드
     // ID로 펀드 존재 여부 확인
-    boolean existsById(Long id);
-
+//    boolean existsById(Long id);
     List<FundsDTO> searchFunds(String keyword);
     List<FundsDTO> findAllFunds();
 }
