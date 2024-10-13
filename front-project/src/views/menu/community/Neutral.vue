b924403ebb0218b4b195c844b208819207204813
rlatneorp@gmail.com <rlatneorp@gmail.com>
2024-10-13 13:20:05 +0900

    [chore] 투자커뮤 크기 조정(글 목록은 아직 수정 전)

--- front-project/src/views/menu/community/Neutral.vue
+++ front-project/src/views/menu/community/Neutral.vue
@@ -53,7 +53,7 @@
 
     </div>
   </div>
-  <button class="moreBtn w-100 text-center" id="kt_widget_5_load_more_btn" >
+  <button class="moreBtn text-center" id="kt_widget_5_load_more_btn" >
         <span class="indicator-label">더보기</span>
         <span class="indicator-progress">Loading...
         <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
@@ -129,7 +129,8 @@ const confirmDelete = async () => {
   font-size: 20px;
 }
 .moreBtn {
-  width: 200px;
+  margin-left: 10px;
+  width: 600px;
   height: 50px;
   border: none;
   border-radius: 20px;
