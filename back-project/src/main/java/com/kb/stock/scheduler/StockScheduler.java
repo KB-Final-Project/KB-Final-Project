b42b2bf8d192e85e2204793e3c5a697b40db73ea
JunSeop Kim <wnqtjq5@gmail.com>
2024-10-14 17:40:19 +0900

    refactor : 토큰 발급로직 수정

--- back-project/src/main/java/com/kb/stock/scheduler/StockScheduler.java
+++ back-project/src/main/java/com/kb/stock/scheduler/StockScheduler.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 
 @Component
@@ -19,10 +20,17 @@ public class StockScheduler {
 
     @Scheduled(cron = "0 */1 * * * *") // 매 1분마다 실행
     public void updateAllStocksScheduled() {
-        logger.info("Starting scheduled update of all stocks at {}", LocalDateTime.now());
+        // 현재 시간을 확인하는 코드
+        LocalDateTime now = LocalDateTime.now();
+        if (now.getHour() >= 16 || now.getDayOfWeek() == DayOfWeek.SATURDAY || now.getDayOfWeek() == DayOfWeek.SUNDAY) {
+            logger.info("현재 시간 {}: 주식 스케줄러가 실행되지 않음 (장 마감 또는 주말).", now);
+            return; // 16시 이후이거나 주말이면 실행하지 않음
+        }
+
+        logger.info("Starting scheduled update of all stocks at {}", now);
         try {
             stockService.updateAllStocks();
-            logger.info("Completed scheduled update of all stocks at {}", LocalDateTime.now());
+            logger.info("Completed scheduled update of all stocks at {}", now);
         } catch (Exception e) {
             logger.error("Error during scheduled update of stocks", e);
         }
