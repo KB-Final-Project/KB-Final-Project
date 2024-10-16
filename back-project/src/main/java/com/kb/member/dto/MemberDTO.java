7e2cc6a73dcafd31996e6aea15f8d09c86d50c74
rlatneorp <rlatneorp@gmail.com>
2024-10-16 10:45:35 +0900

    [chore] 패스워드 수정기능

--- back-project/src/main/java/com/kb/member/dto/MemberDTO.java
+++ back-project/src/main/java/com/kb/member/dto/MemberDTO.java
@@ -5,6 +5,7 @@ import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.sql.Date;
 import java.util.Collection;
@@ -22,6 +23,8 @@ public class MemberDTO {
     private String email;       // 이메일
     private String kakaoId;       // 카카오 아이디
     private String investType;
+
+    MultipartFile avatar;
 
     public Member toMember() {
         return Member.builder().id(id).password(password).name(name).email(email).kakaoId(kakaoId).investType(investType).build();
